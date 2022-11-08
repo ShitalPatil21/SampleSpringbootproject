c2570735f7f25705e0b73b42f1c93c26b389e6c9
shital <pshital2197@gmail.com>
2022-11-08 14:18:25 +0530

    added userUitility in controller

---
+++ src/main/java/com/tcs/demoprojectusingentitymanger/controller/UserUtility.java
@@ -0,0 +1,62 @@
+package com.tcs.demoprojectusingentitymanger.controller;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.tcs.demoprojectusingentitymanger.beans.UserBean;
+import com.tcs.demoprojectusingentitymanger.service.UserService;
+
+@Component
+public class UserUtility {
+
+	@Autowired
+	private static UserService userSe;
+	
+	private static UserService userservice;
+	
+	/*
+	 * @Autowired public UserUtility(UserService userService) { // TODO
+	 * Auto-generated constructor stub UserUtility.userSe=userService;
+	 * 
+	 * 
+	 * }
+	 */
+	@PostConstruct
+	public void init() {
+		System.out.println("Inside Init method");
+		this.userSe = userservice;
+		
+	}
+
+	@Autowired
+	static EntityManagerFactory ef;
+	
+	public static  List<UserBean> getAllUser(){
+		List<UserBean> userList = null;
+		
+		System.out.println("Before EntityManger");
+		EntityManager entityManager = ef.createEntityManager();
+		System.out.println("After EntityManger");
+		try {
+			System.out.println("Insidetry 1");
+			String jpql = "from UserBean";
+			Query query = entityManager.createQuery(jpql);
+			userList = (List<UserBean>) query.getResultList();
+		
+		} catch (Exception e) {
+			e.getMessage();
+		}
+		return userList;
+	}
+	
+	
+	
+}
